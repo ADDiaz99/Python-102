a3b3566145214cfc46901dd438b64cd2fa8b6e3a
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-01-27 15:54:58 -0500

    Python 102

---
+++ 07_functions.py
@@ -0,0 +1,12 @@
+def my_print(text):
+    print(text * 2)
+
+my_print('Este es mi texto')
+
+def mejorque(a, b):
+    print(f'{a} es mejor que {b} ')
+
+
+mejorque('La noche', 'El dia')
+
+
---
+++ 08_return.py
@@ -0,0 +1,7 @@
+def sum_with_range(min, max):
+    sum = 0 
+    for i in range(min, max):
+        sum += i
+    print(sum)
+
+sum_with_range(23, 231)
