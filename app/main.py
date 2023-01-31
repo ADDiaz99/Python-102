899d4ae9de8b5e1f613ff19d7557934c1ad1892c
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-01-31 00:48:13 -0500

    Python 102

--- app/main.py
+++ app/main.py
@@ -1,22 +1,26 @@
 import utils
 
-keys, values = utils.get_population()
-print(keys, values)
-
 data = [
-{
-    'Country' : 'Colombia',
-    'Population' : 500
-},
-{
-    'Country' : 'Bolivia',
-    'Population' : 300
-},
+    {
+        'Country' : 'Colombia',
+        'Population' : 500
+    },
+    {
+        'Country' : 'Bolivia',
+        'Population' : 300
+    },
 
-]
+    ]
 
-country = input('Type a Country => ')
+def run():
+    keys, values = utils.get_population()
+    print(keys, values)
+
+    country = input('Type a Country => ')
 
 
-result = utils.population_by_country(data, country)
-print(result)
+    result = utils.population_by_country(data, country)
+    print(result)
+
+if __name__ == '__main__':
+    run()
