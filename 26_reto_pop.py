91d03228a1437efbc310e92f2e02c2184e8083e0
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-02-03 20:32:18 -0500

    Python 102

---
+++ 26_reto_pop.py
@@ -0,0 +1,12 @@
+colombia = {
+    'Rank' : 'COL',
+    'Country' : 'Colombia',
+    'Capital' : 'Bogota',
+    'Continent' : 'South America',
+    '2022 Population' : 3000,
+    '2020 Population' : 400,
+    '2015 Population' : 500,
+    'World Population Percentage' : 0.12
+    }
+
+population = {key: colombia[key] for key in population}
--- app/main.py
+++ app/main.py
@@ -1,26 +1,30 @@
 import utils
+import read_csv
+import charts
 
-data = [
-    {
-        'Country' : 'Colombia',
-        'Population' : 500
-    },
-    {
-        'Country' : 'Bolivia',
-        'Population' : 300
-    },
-
-    ]
-
+#PARA EXTRAER UNA COLUMNA DE EXCEL:
 def run():
-    keys, values = utils.get_population()
-    print(keys, values)
+    data = read_csv.read_csv('./app/data.csv')
+    # FILTRO PARA CONTINENTES data = list(filter(lambda item: item['Continent'] == 'South America', data))
 
+    countries = list(map(lambda x: x['Country/Territory'], data))
+    percentages = list(map(lambda x: x['World Population Percentage'], data))
+    charts.generate_pie_chart(countries, percentages)
+    
+    '''
     country = input('Type a Country => ')
 
-
     result = utils.population_by_country(data, country)
-    print(result)
+
+    if len(result) > 0:
+        country = result[0]
+        labels, values = utils.get_population(country)
+        charts.generate_bar_chart(labels, values)
+    '''
+
+
+    
+    
 
 if __name__ == '__main__':
     run()
